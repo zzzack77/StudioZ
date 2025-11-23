1165e3a2883719d91e4ba757f3c64251d26abe83
zzzack77 <125263916+zzzack77@users.noreply.github.com>
2025-11-23 13:26:47 +0000

    Level selector works through server so all clients level changes at the same time

--- StudioZ/Assets/Scripts/Managers/GameManager.cs
+++ StudioZ/Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
@@ -9,45 +8,75 @@ public class GameManager : NetworkBehaviour
     private LevelManager levelManager;
 
     public List<GameObject> playerGameObjects = new List<GameObject>();
-
     // A list for the tracked targets in the Cinemachine Target Group
     public List<GameObject> trackedTargets = new List<GameObject>();
-
+    [Header("Debug Level Loading (Editor Only)")]
     [SerializeField] private bool setLevel;
     [SerializeField] private int level;
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
         }
-        else Destroy(gameObject);
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
 
         levelManager = GetComponent<LevelManager>();
     }
+
     private void Update()
     {
+        // Debug manual level change from Inspector
         if (setLevel)
         {
             setLevel = false;
-            LoadLevel(level);
+
+            RequestLoadLevel(level);
         }
     }
-    public void setCurrentLevelTime(float time)
+
+    // --------------------------
+    //  PUBLIC API (Clients call this)
+    // --------------------------
+    public void RequestLoadLevel(int index)
     {
-        if (PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex) > time ||
-            PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex) == 0)
+        LoadLevelServerRpc(index);
+    }
+
+    // --------------------------
+    //  SERVER loads level
+    // --------------------------
+    [ServerRpc(RequireOwnership = false)]
+    private void LoadLevelServerRpc(int index)
+    {
+        LoadLevel(index);             // Server loads the level
+        LoadLevelClientRpc(index);    // Tell all clients to load it
+    }
+
+    // --------------------------
+    //  CLIENTS load level
+    // --------------------------
+    [ClientRpc]
+    private void LoadLevelClientRpc(int index)
+    {
+        // Prevent the host from loading twice
+        if (!IsServer)
         {
-            PlayerDataManager.Instance.SetSingleLevelTime(levelManager.CurrentLevelIndex, time);
+            LoadLevel(index);
         }
-        
     }
-    public float GetCurrentLevelBestTime()
+
+    // --------------------------
+    //  ACTUAL LEVEL LOAD FUNCTION
+    // --------------------------
+    private void LoadLevel(int index)
     {
-        return PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex);
-    }
-    public void LoadLevel(int index) 
-    {
+        // Reset all player positions and respawn
         foreach (GameObject gameObject in playerGameObjects)
         {
             NetworkPlayerMovement networkPlayerMovement = gameObject.GetComponent<NetworkPlayerMovement>();
@@ -57,23 +86,45 @@ public class GameManager : NetworkBehaviour
                 networkPlayerMovement.SpawnPlayer();
             }
         }
-        levelManager.LoadLevel(index); 
-    }
-    public void Reloadlevel() { levelManager.ReloadLevel(); }
 
+        // Load the actual level
+        levelManager.LoadLevel(index);
+    }
+
+    // --------------------------
+    //  MISC FUNCTIONS
+    // --------------------------
+    public void Reloadlevel()
+    {
+        levelManager.ReloadLevel();
+    }
+
+    public void setCurrentLevelTime(float time)
+    {
+        if (PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex) > time ||
+            PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex) == 0)
+        {
+            PlayerDataManager.Instance.SetSingleLevelTime(levelManager.CurrentLevelIndex, time);
+        }
+    }
+
+    public float GetCurrentLevelBestTime()
+    {
+        return PlayerDataManager.Instance.GetSingleLevelTime(levelManager.CurrentLevelIndex);
+    }
+
+    // --------------------------
+    //  PLAYER REGISTRATION
+    // --------------------------
     public void RegisterPlayer(GameObject player)
     {
         if (!playerGameObjects.Contains(player))
-        {
             playerGameObjects.Add(player);
-        }
     }
 
     public void UnregisterPlayer(GameObject player)
     {
         if (playerGameObjects.Contains(player))
-        {
             playerGameObjects.Remove(player);
-        }
     }
 }
